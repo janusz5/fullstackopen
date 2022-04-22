80060d2e07b141bb7d8874c1796397df92d6ff2a
Janusz478 <JFeigel@mailbox.org>
2022-04-22 23:11:32 +0200

    exercise 5.7.

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,31 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const buttonText = visible ? 'hide' : 'view'
+  let contentVisible = { display: visible ? '' : 'none' }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author} <button onClick={() => setVisible(!visible)}>{buttonText}</button>
+      </div>
+      <div style={contentVisible}>
+        <div>{blog.url}</div>
+        <div>{blog.likes}</div>
+        <div>{blog.user.name}</div>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
