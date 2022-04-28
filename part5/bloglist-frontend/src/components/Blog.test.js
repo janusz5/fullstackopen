71e78df3f365f876351af9d6156b26f35d21ae7b
Janusz478 <JFeigel@mailbox.org>
2022-04-28 22:04:04 +0200

    exercise 5.14.

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 describe('Blog tests', () => {
@@ -24,6 +25,29 @@ describe('Blog tests', () => {
 
     const div = container.querySelector('.hiddenContent')
     expect(div).toHaveStyle('display: none')
+    expect(div).toHaveTextContent('testURL')
+    expect(div).toHaveTextContent(0)
+  })
+
+  test('When the button is clicked url and likes are shown', async () => {
+    const blog = {
+      title: 'testTitle',
+      author: 'testAuthor',
+      url: 'testURL',
+      likes: 0,
+      user: {
+        name: 'testUserName'
+      }
+    }
+
+    const { container } = render(<Blog blog={blog} user={{ username: '' }} />)
+
+    const viewButton = await screen.findByText(/view/)
+    const user = userEvent.setup()
+    await user.click(viewButton)
+
+    const div = container.querySelector('.hiddenContent')
+    expect(div).not.toHaveStyle('display: none')
     expect(div).toHaveTextContent('testURL')
     expect(div).toHaveTextContent(0)
   })
