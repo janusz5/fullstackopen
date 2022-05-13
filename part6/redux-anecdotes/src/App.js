4dce00bcd5fd66af78d75861931b5c1f68be8ed8
Janusz478 <JFeigel@mailbox.org>
2022-05-13 05:05:52 +0200

    exercise 6.19.

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -11,7 +11,7 @@ const App = () => {
   const dispatch = useDispatch()
   useEffect(() => {
     anecdoteService.getAll().then(anecdotes => dispatch(setAnecdotes(anecdotes)))
-  }, [])
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps  
 
   return (
     <div>
