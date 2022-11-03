0bb839e1a47a6dfb52ded285146294f415848288
Janusz478 <JFeigel@mailbox.org>
2022-11-03 22:31:15 +0100

    exercise 13.21

--- part13/controllers/users.js
+++ part13/controllers/users.js
@@ -23,7 +23,7 @@ router.get("/:id", async (req, res) => {
       model: Blog,
       as: 'marked_to_read',
       through: {
-        attributes: []
+        attributes: ["id", "read"]
       }
     },{
       model: Blog
