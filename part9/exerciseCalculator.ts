af241cc92270e658737a9584d04e830dfe6ccba9
Janusz478 <JFeigel@mailbox.org>
2022-11-11 14:53:23 +0100

    exercise 9.7.

--- part9/exerciseCalculator.ts
+++ part9/exerciseCalculator.ts
@@ -95,3 +95,5 @@ try {
 }
 
 //console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
+
+export { calculateExercises };
--- part9/index.ts
+++ part9/index.ts
@@ -1,18 +1,42 @@
 import express from "express";
 import { calculateBmi } from "./bmiCalculator";
+import { calculateExercises } from "./exerciseCalculator";
 
 const app = express();
+app.use(express.json());
 
 app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack!");
 });
 
 app.get("/bmi", (req, res) => {
+  if (!req.query.height || !req.query.weight)
+    return res.status(400).json({ error: "parameters missing" });
   const height = Number(req.query.height);
   const weight = Number(req.query.weight);
-  if (!height || !weight)
+  if (isNaN(height) || isNaN(weight))
     return res.status(400).json({ error: "malformatted parameters" });
   return res.json({ weight, height, bmi: calculateBmi(height, weight) });
+});
+
+app.post("/exercises", (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  if (!req.body.daily_exercises || !req.body.target)
+    return res.status(400).json({ error: "parameters missing" });
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  const target = Number(req.body.target);
+
+  const daily_exercises: Array<number> = Array.from(
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-argument
+    req.body.daily_exercises,
+    (x) => Number(x)
+  );
+
+  if (daily_exercises.some(isNaN) || isNaN(target))
+    return res.status(400).json({ error: "malformatted parameters" });
+
+  return res.json(calculateExercises(daily_exercises, target));
 });
 
 const PORT = 3003;
